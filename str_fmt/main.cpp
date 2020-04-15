7d3a1ed0ecd638e2be4efeaeb60d9467a3867736
Lu Ye <luye@microsoft.com>
2020-04-15 14:18:19 +0800

    add swprintf

--- str_fmt/main.cpp
+++ str_fmt/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <bitset>
 #include "snprintf.h"
+#include "swprintf.h"
 
 int main()
 {
     snprintf_null_terminate();
+    swprintf_null_terminate();
     
     return 0;
 }
